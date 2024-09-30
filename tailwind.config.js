d8d50cbe63a44f4c12dcf624ddb4b73493548c78
ARCHANA130 <archana13042004@gmail.com>
2024-09-30 13:20:37 +0530

    Added file

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const { getDisplayName } = require('next/dist/shared/lib/utils');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+        inter: ['Oswald','Inter', 'sans-serif'],
+       
+
+      },
+      colors: {
+        'primary-orange': '#FF5722',
+      }
+    },
+  },
+  plugins: [],
+}
